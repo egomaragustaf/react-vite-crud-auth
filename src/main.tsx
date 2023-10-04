97c060858aa2bca69b90abcb8405046fa7b4b584
Ego Maragustaf <egomaragustaf@gmail.com>
2023-10-05 02:34:38 +0700

    feat: add login routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomeRoute from "./routes/home";
+import Login from "./routes/_auth.login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeRoute />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
---
+++ src/routes/_auth.login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="bg-rose-400">
+      <h1>Login</h1>
+    </div>
+  );
+}
